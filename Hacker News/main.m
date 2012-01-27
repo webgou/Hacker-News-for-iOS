a84542711fef566a5f7683674728aae95c4e8e35
Nathan Campos <eeepc904@gmail.com>
2012-01-27 18:59:40 -0200

    Initial Commit

---
+++ Hacker News/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hacker News
+//
+//  Created by Nathan Campos on 1/27/12.
+//  Copyright Paulino 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
